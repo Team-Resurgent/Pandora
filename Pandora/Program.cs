52a99471b147248b7ccce30f582636a3e541a49c
EqUiNoX <equinox.teamresurgent@gmail.com>
2023-05-23 13:41:08 -0700

    Update: removed hide

--- Pandora/Program.cs
+++ Pandora/Program.cs
@@ -1,21 +1,7 @@
 ﻿using Pandora;
-using System.Runtime.InteropServices;
-
-[DllImport("kernel32.dll")]
-static extern IntPtr GetConsoleWindow();
-
-[DllImport("user32.dll", CharSet = CharSet.Auto)]
-static extern IntPtr SendMessage(IntPtr hWnd, UInt32 Msg, IntPtr wParam, IntPtr lParam);
-
-const int WM_CLOSE = 0x0010;
 
 try
 {
-    if (OperatingSystem.IsWindows())
-    {
-        var handle = GetConsoleWindow();
-        SendMessage(handle, WM_CLOSE, IntPtr.Zero, IntPtr.Zero);
-    }
     var version = "V1.0.4";
     var application = new ApplicationUI();
     application.Start(version);
