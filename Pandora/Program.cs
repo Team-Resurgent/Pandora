ef8aa9c6a3e77ad6bfb292b2f0f0800bd3830ad5
EqUiNoX <equinox.teamresurgent@gmail.com>
2023-05-23 12:44:37 -0700

    Force hide window

--- Pandora/Program.cs
+++ Pandora/Program.cs
@@ -1,7 +1,21 @@
 ﻿using Pandora;
+using System.Runtime.InteropServices;
+
+[DllImport("kernel32.dll")]
+static extern IntPtr GetConsoleWindow();
+
+[DllImport("user32.dll")]
+static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+const int SW_HIDE = 0;
 
 try
 {
+    if (OperatingSystem.IsWindows())
+    {
+        var handle = GetConsoleWindow();
+        ShowWindow(handle, SW_HIDE);
+    }
     var version = "V1.0.4";
     var application = new ApplicationUI();
     application.Start(version);
